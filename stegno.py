d978d50b3c9511156bc539e13a33c2e3bb5a4cc8
rajpravali <52438472+rajpravali@users.noreply.github.com>
2020-01-25 19:59:23 +0530

    Update stegno.py

--- stegno.py
+++ stegno.py
@@ -1,1 +1,14 @@
-hello world
+
+from PIL import Image
+import stepic
+
+im = Image.open ("cat.jpg")
+s=stepic.Steganographer(im)
+im2=s.encode(im ,"hello")
+#im2 = stepic.encode(im, '0987639987069730979076409784690y7689734')
+im2.save('out.jpg')
+im1 = Image.open('cat.jpg')
+s = stepic.decode(im1)
+print(s)
+data = s.decode()
+print(data)
